8559081bd42713787fc834d81ce9496bf8ae8e06
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2023-06-17 21:03:58 +0200

    Simplify the implementation

--- RelativeFileMacro.cmake
+++ RelativeFileMacro.cmake
@@ -13,27 +13,19 @@ This module requires CMake 3.20 or newer.
 Custom functions
 ^^^^^^^^^^^^^^^^
 
-``source_add_relative_file_macro``
+``source_add_relative_file_macro(SRC_FILE)``
   Add a __REL_FILE__ macro definition a source file.
-``target_add_relative_file_macro``
-  Add a __REL_FILE__ macro definition to all sources in the provided target.
+``target_add_relative_file_macro(TARGET)``
+  Add a __REL_FILE__ macro definition to all sources in the provided TARGET.
 
 #]=======================================================================]
 
 # Function that sets the __REL_FILE__ definition for a particular source file
-function(source_add_relative_file_macro SRC_FILE_VAR)
-    # Get the real src file path
-    set(SRC_FILE ${${SRC_FILE_VAR}})
-    # Test if the provided path is relative
-    cmake_path(IS_RELATIVE SRC_FILE_VAR SRC_FILE_IS_REL)
-    if(SRC_FILE_IS_REL)
-        set(SRC_FILE_REL "\"${SRC_FILE}\"")
-    else()
-        # Get relative path
-        # Handles non normalized paths and mixed platform paths :)
-        cmake_path(RELATIVE_PATH ${SRC_FILE_VAR} BASE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} OUTPUT_VARIABLE SRC_FILE_REL)
-        set(SRC_FILE_REL "\"${SRC_FILE}\"")
-    endif()
+function(source_add_relative_file_macro SRC_FILE)
+    # Normalize
+    cmake_path(ABSOLUTE_PATH SRC_FILE NORMALIZE OUTPUT_VARIABLE SRC_FILE_ABSOLUTE)
+    # Get relative path
+    cmake_path(RELATIVE_PATH SRC_FILE_ABSOLUTE BASE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} OUTPUT_VARIABLE SRC_FILE_REL)
     # Set a __FILE__ property for the current source file
     set_property(SOURCE ${SRC_FILE} APPEND PROPERTY COMPILE_DEFINITIONS __REL_FILE__=${SRC_FILE_REL})
 endfunction()
@@ -42,6 +34,6 @@ endfunction()
 function(target_add_relative_file_macro TARGET)
     get_target_property(TARGET_SRCS ${TARGET} SOURCES)
     foreach(SRC ${TARGET_SRCS})
-        source_add_relative_file_macro(SRC)
+        source_add_relative_file_macro(${SRC})
     endforeach()
 endfunction()
